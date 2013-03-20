610c27b4e4337ca4093ce86f38ebb1f73c7192c3
Matheus E. Muller <hello@memuller.com>
2013-03-20 02:14:30 -0300

    Properly handles content- templates for custom post types.

--- single.php
+++ single.php
@@ -1,1 +1,2 @@
-<?php get_template_part('templates/content', 'single'); ?>
+<?php $format = have_posts() && get_post_type() != 'post' ? 'single-'. get_post_type() : ''; ?>
+<?php get_template_part('templates/content', $format); ?>
--- templates/content-single-pedia.php
+++ templates/content-single-pedia.php
@@ -1,6 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
+      <h1>Hey! This is a pedia item.</h1>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
